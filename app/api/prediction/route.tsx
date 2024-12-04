631b544607bfd8e595ecc3d97f7d96d17d3a3c6f
yasu691 <yasu691metroid@gmail.com>
2024-12-04 23:29:31 +0900

    Geminiを呼び出すように変更

--- app/api/prediction/route.tsx
+++ app/api/prediction/route.tsx
@@ -22,39 +22,38 @@ export async function POST(req: Request) {
     // リクエストボディからプロンプトを抽出
     const { prompt } = await req.json();
 
-    // OpenAI APIキーが設定されているか確認
-    if (!process.env.OPENAI_API_KEY) {
-      console.error("OPENAI_API_KEY is not set");
-      throw new Error("OPENAI_API_KEY is not set");
+    // GeminiのAPIキーが設定されているか確認
+    if (!process.env.GEMINI_API_KEY) {
+      console.error("GEMINI_API_KEY is not set");
+      throw new Error("GEMINI_API_KEY is not set");
     }
 
+    // OpenAIクライアントを作成してGeminiの設定を読み込ませる
+    const openai = new OpenAI({
+      apiKey: process.env.GEMINI_API_KEY,
+      baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+    })
+
     console.log("Sending request to OpenAI API");
-    // OpenAI APIにリクエストを送信
-    const response = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
-      {
-        model: "gpt-4o-mini", // 使用するモデル
-        messages: [
-          { role: "system", content: systemPrompt }, // システムメッセージ
-          { role: "user", content: prompt }, // ユーザーのプロンプト
-        ],
-        max_tokens: 10, // 生成するトークンの最大数
-        n: 1, // 生成する回答の数
-        stop: null, // 生成を停止する条件（ここではnull）
-        temperature: 0.7, // 生成の多様性（0.0-1.0）
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`, // 認証ヘッダー
-        },
-      }
-    );
+
+    // bodyの作成
+    const body: OpenAI.ChatCompletionCreateParams = {
+      model: "gemini-1.5-flash", // 使用するモデル
+      messages: [
+        { role: "system", content: systemPrompt }, // システムメッセージ
+        { role: "user", content: prompt }, // ユーザーのプロンプト
+      ],
+      max_tokens: 10,
+      temperature: 0.1
+    };
+
+    // Gemini APIにリクエストを送信
+    const response = await openai.chat.completions.create(body);
 
     console.log("Received response from OpenAI API");
     // APIレスポンスから予測テキストを抽出し、JSONとして返す
     return NextResponse.json({
-      prediction: response.data.choices[0].message.content.trim(),
+      prediction: response.choices[0].message.content,
     });
   } catch (error) {
     console.error("Error in API route:", error);
